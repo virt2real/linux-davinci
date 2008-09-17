b392a90a5c2cebf2da1648f185302817de93acff
Sudhakar Rajashekhara <sudhakar.raj@ti.com>
2008-09-17 12:03:42 +0530

    ARM: DaVinci: replace dm6443 with dm644x
    Replaces occurrences of dm6443 with dm644x.
    
    The cpu is decided based on the system_rev variable and it is being
    compared with 6446 (Refer to arch/arm/mach-davinci/id.c for details).
    So in the current tree, the function cpu_is_davinci_dm6443 will never
    be true.
    
    Signed-off-by: Sudhakar Rajashekhara <sudhakar.raj@ti.com>

--- arch/arm/mach-davinci/include/mach/cpu.h
+++ arch/arm/mach-davinci/include/mach/cpu.h
@@ -25,7 +25,7 @@ static inline int cpu_is_davinci_dm ##type (void)	\
 }
 
 /* following generates the cpu_is_davinci_dmxxx */
-IS_DAVINCI_CPU(6443, 0x6443)	/* cpu_is_davinci_dm6443() */
+IS_DAVINCI_CPU(644x, 0x6446)	/* cpu_is_davinci_dm644x() */
 IS_DAVINCI_CPU(6467, 0x6467)	/* cpu_is_davinci_dm6467() */
 IS_DAVINCI_CPU(350, 0x350)	/* cpu_is_davinci_dm350() */
 
