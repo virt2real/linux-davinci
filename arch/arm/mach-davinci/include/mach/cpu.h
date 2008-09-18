72b4923be58a36c3fa70f81836eda2fa14e8dba5
Sudhakar Rajashekhara <sudhakar.raj@ti.com>
2008-09-18 21:03:34 +0530

    ARM: DaVinci: replace dm6467 with dm646x
    Replaces occurrence of dm6467 with dm646x.
    
    Signed-off-by: Sudhakar Rajashekhara <sudhakar.raj@ti.com>

--- arch/arm/mach-davinci/include/mach/cpu.h
+++ arch/arm/mach-davinci/include/mach/cpu.h
@@ -26,7 +26,7 @@ static inline int cpu_is_davinci_dm ##type (void)	\
 
 /* following generates the cpu_is_davinci_dmxxx */
 IS_DAVINCI_CPU(644x, 0x6446)	/* cpu_is_davinci_dm644x() */
-IS_DAVINCI_CPU(6467, 0x6467)	/* cpu_is_davinci_dm6467() */
+IS_DAVINCI_CPU(646x, 0x6467)	/* cpu_is_davinci_dm646x() */
 IS_DAVINCI_CPU(350, 0x350)	/* cpu_is_davinci_dm350() */
 
 #endif
