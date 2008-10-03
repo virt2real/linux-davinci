1c122dfa941bdacfdddc015c2fb6636f3a3d5f39
Troy Kisky <troy.kisky@boundarydevices.com>
2008-10-03 14:52:34 -0700

    ARM: DaVinci: IRAM alloc/free routines
    This adds IRAM allocation and free routines which
    will be used to fix audio underrun/overrun problems.
    
    Signed-off-by: Troy Kisky <troy.kisky@boundarydevices.com>

--- arch/arm/mach-davinci/include/mach/edma.h
+++ arch/arm/mach-davinci/include/mach/edma.h
@@ -560,4 +560,7 @@ void davinci_free_dma(int lch);
  * DMA channel
  **/
 void davinci_dma_getposition(int lch, dma_addr_t *src, dma_addr_t *dst);
+
+int davinci_alloc_iram(unsigned size);
+void davinci_free_iram(unsigned addr, unsigned size);
 #endif
