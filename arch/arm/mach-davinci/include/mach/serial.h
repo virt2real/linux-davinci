b79a3d23a171f25097beb84fd9bbfef809145f0e
Mark A. Greer <mgreer@mvista.com>
2009-03-03 15:51:43 -0700

    ARM: DaVinci - Clean up whitespace in serial.h
    Replace spaces with tabs.
    
    Signed-off-by: Mark A. Greer <mgreer@mvista.com>
    Signed-off-by: Kevin Hilman <khilman@deeprootsystems.com>

--- arch/arm/mach-davinci/include/mach/serial.h
+++ arch/arm/mach-davinci/include/mach/serial.h
@@ -13,17 +13,17 @@
 
 #include <mach/io.h>
 
-#define DAVINCI_MAX_NR_UARTS 3
-#define DAVINCI_UART0_BASE   (IO_PHYS + 0x20000)
-#define DAVINCI_UART1_BASE   (IO_PHYS + 0x20400)
-#define DAVINCI_UART2_BASE   (IO_PHYS + 0x20800)
+#define DAVINCI_MAX_NR_UARTS	3
+#define DAVINCI_UART0_BASE	(IO_PHYS + 0x20000)
+#define DAVINCI_UART1_BASE	(IO_PHYS + 0x20400)
+#define DAVINCI_UART2_BASE	(IO_PHYS + 0x20800)
 
-#define DM355_UART2_BASE     (IO_PHYS + 0x206000)
+#define DM355_UART2_BASE	(IO_PHYS + 0x206000)
 
 /* DaVinci UART register offsets */
-#define UART_DAVINCI_PWREMU           0x0c
-#define UART_DM646X_SCR	              0x10
-#define UART_DM646X_SCR_TX_WATERMARK  0x08
+#define UART_DAVINCI_PWREMU		0x0c
+#define UART_DM646X_SCR			0x10
+#define UART_DM646X_SCR_TX_WATERMARK	0x08
 
 struct davinci_uart_config {
 	/* Bit field of UARTs present; bit 0 --> UART1 */
