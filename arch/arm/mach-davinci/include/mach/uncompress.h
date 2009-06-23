23213a0e112fabf55327e65c461959003852fb69
Kevin Hilman <khilman@deeprootsystems.com>
2009-06-23 13:01:22 -0700

    Revert "ARM: da830 - Add support for DA830/OMAP-L137 EVM"
    This reverts commit 32b9fa0df29d31e01b28c3296c3b0ed744a775da.

--- arch/arm/mach-davinci/include/mach/uncompress.h
+++ arch/arm/mach-davinci/include/mach/uncompress.h
@@ -21,10 +21,8 @@ static u32 *uart;
 
 static u32 *get_uart_base(void)
 {
-	if (__machine_arch_type == MACH_TYPE_DAVINCI_DA8XX_EVM)
-		return (u32 *)DA830_UART2_BASE;
-	else
-		return (u32 *)DAVINCI_UART0_BASE;
+	/* Add logic here for new platforms, using __macine_arch_type */
+	return (u32 *)DAVINCI_UART0_BASE;
 }
 
 /* PORT_16C550A, in polled non-fifo mode */
