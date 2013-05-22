103ea8c0c68aceefa30148881e1a443b6c1733b5
AV2R <avs@virt2real.com>
2013-05-22 23:35:03 +0400

    Commit fo generated files

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 5 */
+/* This file is auto generated, version 14 */
 /*  PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#5 PREEMPT Fri May 17 16:37:22 MSD 2013"
+#define UTS_VERSION "#14 PREEMPT Wed May 22 12:38:14 MSK 2013"
 #define LINUX_COMPILE_BY "root"
-#define LINUX_COMPILE_HOST "v2r-vm-new"
+#define LINUX_COMPILE_HOST "ubuntu-desktop"
 #define LINUX_COMPILER "gcc version 4.4.1 (Sourcery G++ Lite 2010q1-202) "
