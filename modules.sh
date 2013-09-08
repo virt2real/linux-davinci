22651e6f79e9958f0eb930da1f0a1524764d7a69
AV2R <avs@virt2real.com>
2013-09-08 08:22:22 +0400

    Compiler version upgraded

--- modules.sh
+++ modules.sh
@@ -1,4 +1,4 @@
-export PATH=/opt/codesourcery/arm-2010q1/bin:$PATH
+export PATH=/opt/codesourcery/arm-2013q1/bin:$PATH
 echo $PATH
 make ARCH=arm CROSS_COMPILE=arm-none-linux-gnueabi- modules
 make ARCH=arm CROSS_COMPILE=arm-none-linux-gnueabi- modules_install
