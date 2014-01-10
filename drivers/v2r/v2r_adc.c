3dd61b7ff487a2b5db8fb1b5555f54872770bfed
Gol <root@virt2real.(none)>
2014-01-10 16:52:04 -0500

    New v2r_adc driver version

--- drivers/v2r/v2r_adc.c
+++ drivers/v2r/v2r_adc.c
@@ -13,161 +13,233 @@
 #include <linux/io.h>
 #include <linux/uaccess.h>
 #include "v2r_adc.h"
+
+#define DEVICE_NAME "v2r_adc"
 #define ADC_VERSION "1.0"
 
- void *dm365_adc_base;
+void *dm365_adc_base;
  
- int adc_single(unsigned int channel)
- {
-   if (channel >= ADC_MAX_CHANNELS)
-     return -1;
+int adc_single(unsigned int channel) {
 
-   //select channel
-   iowrite32(1 << channel,dm365_adc_base + DM365_ADC_CHSEL);
+	if (channel >= ADC_MAX_CHANNELS)
+		return -1;
 
-   //start coversion
-   iowrite32(DM365_ADC_ADCTL_BIT_START,dm365_adc_base + DM365_ADC_ADCTL);
+	//select channel
+	iowrite32(1 << channel,dm365_adc_base + DM365_ADC_CHSEL);
 
-   // Wait for conversion to start
-   while (!(ioread32(dm365_adc_base + DM365_ADC_ADCTL) & DM365_ADC_ADCTL_BIT_BUSY)){
-     cpu_relax();
-   }
+	//start coversion
+	iowrite32(DM365_ADC_ADCTL_BIT_START,dm365_adc_base + DM365_ADC_ADCTL);
 
-   // Wait for conversion to be complete.
-   while ((ioread32(dm365_adc_base + DM365_ADC_ADCTL) & DM365_ADC_ADCTL_BIT_BUSY)){
-     cpu_relax();
-   }
+	// Wait for conversion to start
+	while (!(ioread32(dm365_adc_base + DM365_ADC_ADCTL) & DM365_ADC_ADCTL_BIT_BUSY)){
+		cpu_relax();
+	}
 
-   return ioread32(dm365_adc_base + DM365_ADC_AD0DAT + 4 * channel);
- }
+	// Wait for conversion to be complete.
+	while ((ioread32(dm365_adc_base + DM365_ADC_ADCTL) & DM365_ADC_ADCTL_BIT_BUSY)){
+		cpu_relax();
+	}
 
- //static spinlock_t adc_lock = SPIN_LOCK_UNLOCKED;
- DEFINE_SPINLOCK(adc_lock); // changed by Gol
+	return ioread32(dm365_adc_base + DM365_ADC_AD0DAT + 4 * channel);
+}
 
- static void adc_read_block(unsigned short *data, size_t length)
- {
-   int i;
-   spin_lock_irq(&adc_lock);
-     for(i = 0; i < length; i++) {
-       data [i] = adc_single(i);
-     }
-   spin_unlock_irq(&adc_lock);
- }
- 
- #ifndef CONFIG_PROC_FS
- static int adc_add_proc_fs(void)
- {
-   return 0;
- }
+//static spinlock_t adc_lock = SPIN_LOCK_UNLOCKED;
+DEFINE_SPINLOCK(adc_lock); // changed by Gol
 
- #else
- static int adc_proc_read(struct seq_file *seq, void *offset)
- {
-   int i;
-   unsigned short data [ADC_MAX_CHANNELS];
+static void adc_read_block(unsigned short *data, size_t length) {
 
-   adc_read_block(data,ADC_MAX_CHANNELS);
+	int i;
 
-   for(i = 0; i < ADC_MAX_CHANNELS; i++) {
-     seq_printf(seq, "0x%04X\n", data[i]);
-   }
+	spin_lock_irq(&adc_lock);
 
-   return 0;
- }
+	for(i = 0; i < length; i++) {
+		data[i] = adc_single(i);
+	}
 
- static int adc_proc_open(struct inode *inode, struct file *file)
- {
-   return single_open(file, adc_proc_read, NULL);
- }
+	spin_unlock_irq(&adc_lock);
 
- static const struct file_operations adc_proc_fops = {
-   .owner    = THIS_MODULE,
-   .open   = adc_proc_open,
-   .read   = seq_read,
-   .release  = single_release,
- };
+}
 
- static int adc_add_proc_fs(void)
- {
-   if (!proc_create("driver/v2r_adc", 0, NULL, &adc_proc_fops))
-     return -ENOMEM;
-   return 0;
- }
+/**********************************************************************
+ for PROC_FS 
+**********************************************************************/
 
- #endif /* CONFIG_PROC_FS */
+#ifndef CONFIG_PROC_FS
 
- static ssize_t adc_read(struct file *file, char __user *buf, size_t count, loff_t *ppos)
- {
-   unsigned short data [ADC_MAX_CHANNELS];
+static int pins_add_proc_fs(void) {
+	return 0;
+}
 
-   if (count < sizeof(unsigned short))
-     return -ETOOSMALL;
+static int pins_remove_proc_fs(void) {
+	return 0;
+}
 
-   adc_read_block(data,ADC_MAX_CHANNELS);
+#else
 
-   if (copy_to_user(buf, data, count))
-     return -EFAULT;
+static struct proc_dir_entry *proc_parent;
+static struct proc_dir_entry *proc_entry;
+static s32 proc_write_entry[ADC_MAX_CHANNELS];
 
-   return count;
+static int adc_read_proc (int adc_number, char *buf, char **start, off_t offset, int count, int *eof, void *data ) {
 
- }
- static int adc_open(struct inode *inode, struct file *file)
- {
-   return 0;
- }
- static int adc_release(struct inode *inode, struct file *file)
- {
-   return 0;
- }
- static const struct file_operations adc_fops = {
-   .owner    = THIS_MODULE,
-   .read   = adc_read,
-   .open   = adc_open,
-   .release  = adc_release,
- };
+	int value = 0;
+	int len = 0;
 
- static struct miscdevice adc_dev = {
-   NVRAM_MINOR,
-   "v2r_adc",
-   &adc_fops
- };
+	if (adc_number >= ADC_MAX_CHANNELS) {
+		printk("%s: wrong  channel (%d)\n", DEVICE_NAME, adc_number);
+		return -ENOMEM;
+	}
+
+	
+	value = adc_single(adc_number);
+	len = sprintf(buf, "%d", value);
+
+	return len;
+
+}
+
+/* *i'd line to use array init, but i don't know how get file id from unified functions */
+
+static int adc_read_proc_0 (char *buf, char **start, off_t offset, int count, int *eof, void *data ) { return adc_read_proc (0, buf, start, offset, count, eof, data); }
+static int adc_read_proc_1 (char *buf, char **start, off_t offset, int count, int *eof, void *data ) { return adc_read_proc (1, buf, start, offset, count, eof, data); }
+static int adc_read_proc_2 (char *buf, char **start, off_t offset, int count, int *eof, void *data ) { return adc_read_proc (2, buf, start, offset, count, eof, data); }
+static int adc_read_proc_3 (char *buf, char **start, off_t offset, int count, int *eof, void *data ) { return adc_read_proc (3, buf, start, offset, count, eof, data); }
+static int adc_read_proc_4 (char *buf, char **start, off_t offset, int count, int *eof, void *data ) { return adc_read_proc (4, buf, start, offset, count, eof, data); }
+static int adc_read_proc_5 (char *buf, char **start, off_t offset, int count, int *eof, void *data ) { return adc_read_proc (5, buf, start, offset, count, eof, data); }
 
 
- static int adc_init_module(void)
- {
-   int ret;
-   ret = misc_register(&adc_dev);
-   if (ret) {
-     printk(KERN_ERR "v2r_adc: can't misc_register on minor=%d\n",
-         NVRAM_MINOR);
-     return ret;
-   }
-   ret = adc_add_proc_fs();
-   if (ret) {
-     misc_deregister(&adc_dev);
-     printk(KERN_ERR "v2r_adc: can't create /proc/driver/v2r_adc\n");
-     return ret;
-   }
-   if (!devm_request_mem_region(adc_dev.this_device, DM365_ADC_BASE,64,"v2r_adc"))
-      return -EBUSY;
+static int adc_remove_proc_fs(void) {
 
-   dm365_adc_base = devm_ioremap_nocache(adc_dev.this_device,DM365_ADC_BASE, 64);// Physical address,Number of bytes to be mapped.
-   if (!dm365_adc_base)
-     return -ENOMEM;
+	int i;
+	char fn[10];
 
-   printk(KERN_INFO "TI Davinci ADC v" ADC_VERSION "\n");
-   return 0;
- }
+	for (i = 0; i < ADC_MAX_CHANNELS; i++) {
 
- static void adc_exit_module(void)
- {
-   remove_proc_entry("driver/v2r_adc", NULL);
-   misc_deregister(&adc_dev);
-        printk( KERN_DEBUG "Module v2r_adc exit\n" );
- }
+		if (proc_write_entry[i]) { 
+			sprintf(fn, "%d", i);
+			remove_proc_entry(fn, proc_parent);
+		}
 
- module_init(adc_init_module);
- module_exit(adc_exit_module);
- //MODULE_DESCRIPTION("TI Davinci Dm365 Virt2real ADC");
- //MODULE_AUTHOR("Shlomo Kut,,, (shl...@infodraw.com)");
- //MODULE_LICENSE("GPL");
+	}
+
+	/* remove proc_fs directory */
+	remove_proc_entry("v2r_adc",NULL);
+
+	return 0;
+}
+
+static int adc_add_proc_fs(void) {
+
+	proc_parent = proc_mkdir("v2r_adc", NULL);
+
+	if (!proc_parent) {
+		printk("%s: error creating proc entry (/proc/v2r_adc)\n", DEVICE_NAME);
+		return 1;
+	}
+
+	proc_entry = create_proc_entry("0", 0666, proc_parent); if (proc_entry) { proc_entry-> read_proc = adc_read_proc_0; proc_write_entry[0] = (s32) proc_entry; }
+	proc_entry = create_proc_entry("1", 0666, proc_parent); if (proc_entry) { proc_entry-> read_proc = adc_read_proc_1; proc_write_entry[1] = (s32) proc_entry; }
+	proc_entry = create_proc_entry("2", 0666, proc_parent); if (proc_entry) { proc_entry-> read_proc = adc_read_proc_2; proc_write_entry[2] = (s32) proc_entry; }
+	proc_entry = create_proc_entry("3", 0666, proc_parent); if (proc_entry) { proc_entry-> read_proc = adc_read_proc_3; proc_write_entry[3] = (s32) proc_entry; }
+	proc_entry = create_proc_entry("4", 0666, proc_parent); if (proc_entry) { proc_entry-> read_proc = adc_read_proc_4; proc_write_entry[4] = (s32) proc_entry; }
+	proc_entry = create_proc_entry("5", 0666, proc_parent); if (proc_entry) { proc_entry-> read_proc = adc_read_proc_5; proc_write_entry[5] = (s32) proc_entry; }
+	return 0;
+}
+
+#endif /* CONFIG_PROC_FS */
+
+
+/**********************************************************************
+ end for PROC_FS 
+**********************************************************************/
+
+
+static ssize_t adc_read(struct file *file, char __user *buf, size_t count, loff_t *ppos) {
+
+	unsigned short data [ADC_MAX_CHANNELS];
+
+	if (count < sizeof(unsigned short))
+		return -ETOOSMALL;
+
+	adc_read_block(data,ADC_MAX_CHANNELS);
+
+	if (copy_to_user(buf, data, count))
+		return -EFAULT;
+
+	return count;
+
+}
+
+static int adc_open(struct inode *inode, struct file *file) {
+	return 0;
+}
+
+
+static int adc_release(struct inode *inode, struct file *file) {
+	return 0;
+}
+
+
+static const struct file_operations adc_fops = {
+	.owner    = THIS_MODULE,
+	.read   = adc_read,
+	.open   = adc_open,
+	.release  = adc_release,
+};
+
+
+static struct miscdevice adc_dev = {
+
+	NVRAM_MINOR,
+	"v2r_adc",
+	&adc_fops
+
+};
+
+
+static int adc_init_module(void) {
+
+	int ret;
+	ret = misc_register(&adc_dev);
+
+	printk("%s: device is created successfully\n", DEVICE_NAME);
+
+	if (ret) {
+		printk(KERN_ERR "%s: can't misc_register on minor=%d\n", DEVICE_NAME, NVRAM_MINOR);
+		return ret;
+	}
+
+	ret = adc_add_proc_fs();
+
+	if (ret) {
+		misc_deregister(&adc_dev);
+		printk(KERN_ERR "%s: can't create /proc/driver/v2r_adc\n", DEVICE_NAME);
+		return ret;
+	}
+
+	if (!devm_request_mem_region(adc_dev.this_device, DM365_ADC_BASE, 64, "v2r_adc"))
+		return -EBUSY;
+
+	dm365_adc_base = devm_ioremap_nocache(adc_dev.this_device, DM365_ADC_BASE, 64); // Physical address,Number of bytes to be mapped.
+
+	if (!dm365_adc_base)
+		return -ENOMEM;
+
+	return 0;
+}
+
+static void adc_exit_module(void) {
+
+	adc_remove_proc_fs();
+
+	misc_deregister(&adc_dev);
+
+        printk("%s: exit\n", DEVICE_NAME);
+
+}
+
+module_init(adc_init_module);
+module_exit(adc_exit_module);
+MODULE_DESCRIPTION("Virt2real ADC driver module version 0.5");
+MODULE_AUTHOR("Shlomo Kut,,, (shl...@infodraw.com)");
+MODULE_LICENSE("GPL2");
