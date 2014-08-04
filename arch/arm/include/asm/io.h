ad763d14b3aa4dbb774aa8786cf7f335d7d10882
Alexander Shadrin <shadrin_av@list.ru>
2014-08-04 20:23:49 +0400

    Removed odd line at the end of file

--- arch/arm/include/asm/io.h
+++ arch/arm/include/asm/io.h
@@ -400,4 +400,3 @@ extern void register_isa_ports(unsigned int mmio, unsigned int io,
 #endif	/* __KERNEL__ */
 #endif	/* __ASM_ARM_IO_H */
 
-#define davinci_writel(v,a)    (*(volatile unsigned int   *)IO_ADDRESS(a) = (v))
