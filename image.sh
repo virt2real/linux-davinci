bd644232b807d7680b5995f7aa6ed2f542586fa8
Dolin Sergey <dlinyj@gmail.com>
2014-09-22 15:44:53 +0400

    + RTO driver
    This includes following commits:
    первая проба RTO дравйвера
    Вторая проба RTO
    Новая ветка
    rto работает!!!
    Дополнения, которые позволили работать DMA
    Переписанно всё DMA. Добавленна поддержка ciclyc DMA.  Всё взято из старших ядер. Включена работа FIQ
    Добавлен модуль ppmsum
    Добавлеy ppmsum
    Добавлена документация Documentation/v2r/ppmsum.txt
    Исправленн драйвер ppmsum, изменена документация

--- image.sh
+++ image.sh
@@ -1,4 +1,4 @@
-export PATH=/opt/codesourcery/arm-2013q1/bin:./../uboot/tools:$PATH
+export PATH=/home/dlinyj/install-sdk/codesourcery/arm-2013.05/bin:./../uboot/tools:$PATH
 echo $PATH
 make -j4 ARCH=arm CROSS_COMPILE=arm-none-linux-gnueabi- CONFIG_DEBUG_SECTION_MISMATCH=y uImage
 
