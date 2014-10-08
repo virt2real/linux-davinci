73aa45bcb26ce45d21e68fcfb5e97f9a93524a1a
Dolin Sergey <dlinyj@gmail.com>
2014-10-08 18:07:57 +0400

    Добавлен модуль ppmsum

--- modules.sh
+++ modules.sh
@@ -1,4 +1,5 @@
-export PATH=/opt/codesourcery/arm-2013q1/bin:$PATH
+#export PATH=/opt/codesourcery/arm-2013q1/bin:$PATH
+export PATH=/home/dlinyj/install-sdk/codesourcery/arm-2013.05/bin:./../uboot/tools:$PATH
 echo $PATH
 make ARCH=arm CROSS_COMPILE=arm-none-linux-gnueabi- modules
 make ARCH=arm CROSS_COMPILE=arm-none-linux-gnueabi- modules_install
