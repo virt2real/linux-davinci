8ee776f4cdc7caa8780135689b53fb300241740d
Alexey Ignatov <lexszero@gmail.com>
2016-01-21 12:09:51 +0300

    iio: dht11: Count only falling edges to improve robustness

--- drivers/iio/humidity/dht11.c
+++ drivers/iio/humidity/dht11.c
@@ -41,16 +41,20 @@
 
 #define DHT11_DATA_VALID_TIME	2000000000  /* 2s in ns */
 
-#define DHT11_EDGES_PREAMBLE 4
 #define DHT11_BITS_PER_READ 40
-#define DHT11_EDGES_PER_READ (2*DHT11_BITS_PER_READ + DHT11_EDGES_PREAMBLE + 1)
+/* We care only about falling edges */
+#define DHT11_EDGES_PER_READ (DHT11_BITS_PER_READ + 2)
 
-/* Data transmission timing (nano seconds) */
-#define DHT11_START_TRANSMISSION	18  /* ms */
-#define DHT11_SENSOR_RESPONSE	80000
-#define DHT11_START_BIT		50000
-#define DHT11_DATA_BIT_LOW	27000
-#define DHT11_DATA_BIT_HIGH	70000
+/* Data transmission timing */
+#define DHT11_START_TRANSMISSION	30  /* ms */
+
+/* Each bit on wire consists of 'start' part - 50 us low state,
+ * followed by high state staying for 26-28 us for '0' and 70 us for '1'.
+ * It's good to have a big tolerance for 0/1 distinguishing threshold.
+ * 60 us works fine.
+ */
+#define DHT11_BIT_START		50000  /* us */
+#define DHT11_BIT_THRESHOLD	50000  /* us */
 
 struct dht11 {
 	struct device			*dev;
@@ -67,58 +71,33 @@ struct dht11 {
 
 	/* num_edges: -1 means "no transmission in progress" */
 	int				num_edges;
-	struct {s64 ts; int value; }	edges[DHT11_EDGES_PER_READ];
+	s64				edges[DHT11_EDGES_PER_READ];
 };
 
-static unsigned char dht11_decode_byte(int *timing, int threshold)
+static unsigned char dht11_decode_byte(s64 *edges, int offset)
 {
 	unsigned char ret = 0;
 	int i;
 
 	for (i = 0; i < 8; ++i) {
 		ret <<= 1;
-		if (timing[i] >= threshold)
-			++ret;
+		if (edges[offset+i+1] - edges[offset+i] > DHT11_BIT_START + DHT11_BIT_THRESHOLD)
+			ret |= 1;
 	}
 
 	return ret;
 }
 
-static int dht11_decode(struct dht11 *dht11, int offset)
+static int dht11_decode(struct dht11 *dht11)
 {
-	int i, t, timing[DHT11_BITS_PER_READ], threshold,
-		timeres = DHT11_SENSOR_RESPONSE;
 	unsigned char temp_int, temp_dec, hum_int, hum_dec, checksum;
+	s64 *edges = dht11->edges + 1;
 
-	/* Calculate timestamp resolution */
-	for (i = 1; i < dht11->num_edges; ++i) {
-		t = dht11->edges[i].ts - dht11->edges[i-1].ts;
-		if (t > 0 && t < timeres)
-			timeres = t;
-	}
-	if (2*timeres > DHT11_DATA_BIT_HIGH) {
-		pr_err("dht11: timeresolution %d too bad for decoding\n",
-			timeres);
-		return -EIO;
-	}
-	threshold = DHT11_DATA_BIT_HIGH / timeres;
-	if (DHT11_DATA_BIT_LOW/timeres + 1 >= threshold)
-		pr_err("dht11: WARNING: decoding ambiguous\n");
-
-	/* scale down with timeres and check validity */
-	for (i = 0; i < DHT11_BITS_PER_READ; ++i) {
-		t = dht11->edges[offset + 2*i + 2].ts -
-			dht11->edges[offset + 2*i + 1].ts;
-		if (!dht11->edges[offset + 2*i + 1].value)
-			return -EIO;  /* lost synchronisation */
-		timing[i] = t / timeres;
-	}
-
-	hum_int = dht11_decode_byte(timing, threshold);
-	hum_dec = dht11_decode_byte(&timing[8], threshold);
-	temp_int = dht11_decode_byte(&timing[16], threshold);
-	temp_dec = dht11_decode_byte(&timing[24], threshold);
-	checksum = dht11_decode_byte(&timing[32], threshold);
+	hum_int = dht11_decode_byte(edges, 0);
+	hum_dec = dht11_decode_byte(edges, 8);
+	temp_int = dht11_decode_byte(edges, 16);
+	temp_dec = dht11_decode_byte(edges, 24);
+	checksum = dht11_decode_byte(edges, 32);
 
 	if (((hum_int + hum_dec + temp_int + temp_dec) & 0xff) != checksum)
 		return -EIO;
@@ -141,6 +120,25 @@ static int dht11_decode(struct dht11 *dht11, int offset)
 	return 0;
 }
 
+/*
+ * IRQ handler called on GPIO edges
+ */
+static irqreturn_t dht11_handle_irq(int irq, void *data)
+{
+	struct iio_dev *iio = data;
+	struct dht11 *dht11 = iio_priv(iio);
+
+	/* TODO: Consider making the handler safe for IRQ sharing */
+	if (dht11->num_edges < DHT11_EDGES_PER_READ && dht11->num_edges >= 0) {
+		dht11->edges[dht11->num_edges++] = iio_get_time_ns();
+
+		if (dht11->num_edges >= DHT11_EDGES_PER_READ)
+			complete(&dht11->completion);
+	}
+
+	return IRQ_HANDLED;
+}
+
 static int dht11_read_raw(struct iio_dev *iio_dev,
 			const struct iio_chan_spec *chan,
 			int *val, int *val2, long m)
@@ -152,14 +150,18 @@ static int dht11_read_raw(struct iio_dev *iio_dev,
 	if (dht11->timestamp + DHT11_DATA_VALID_TIME < iio_get_time_ns()) {
 		reinit_completion(&dht11->completion);
 
-		dht11->num_edges = 0;
 		ret = gpio_direction_output(dht11->gpio, 0);
 		if (ret)
 			goto err;
 		msleep(DHT11_START_TRANSMISSION);
+
+		dht11->num_edges = 0;
+
 		ret = gpio_direction_input(dht11->gpio);
-		if (ret)
+		if (ret) {
+			free_irq(dht11->irq, iio_dev);
 			goto err;
+		}
 
 		ret = wait_for_completion_killable_timeout(&dht11->completion,
 								 HZ);
@@ -172,10 +174,7 @@ static int dht11_read_raw(struct iio_dev *iio_dev,
 		if (ret < 0)
 			goto err;
 
-		ret = dht11_decode(dht11,
-				dht11->num_edges == DHT11_EDGES_PER_READ ?
-					DHT11_EDGES_PREAMBLE :
-					DHT11_EDGES_PREAMBLE - 2);
+		ret = dht11_decode(dht11);
 		if (ret)
 			goto err;
 	}
@@ -197,27 +196,6 @@ static const struct iio_info dht11_iio_info = {
 	.driver_module		= THIS_MODULE,
 	.read_raw		= dht11_read_raw,
 };
-
-/*
- * IRQ handler called on GPIO edges
-*/
-static irqreturn_t dht11_handle_irq(int irq, void *data)
-{
-	struct iio_dev *iio = data;
-	struct dht11 *dht11 = iio_priv(iio);
-
-	/* TODO: Consider making the handler safe for IRQ sharing */
-	if (dht11->num_edges < DHT11_EDGES_PER_READ && dht11->num_edges >= 0) {
-		dht11->edges[dht11->num_edges].ts = iio_get_time_ns();
-		dht11->edges[dht11->num_edges++].value =
-						gpio_get_value(dht11->gpio);
-
-		if (dht11->num_edges >= DHT11_EDGES_PER_READ)
-			complete(&dht11->completion);
-	}
-
-	return IRQ_HANDLED;
-}
 
 static const struct iio_chan_spec dht11_chan_spec[] = {
 	{ .type = IIO_TEMP,
